e9ea1cdc12e678ea76ee1965d7c6992a98fc2675
github username <github email address>
2020-03-02 14:14:50 -0600

    added serializers

---
+++ watchlist-backend/app/serializers/viewer_serializer.rb
@@ -0,0 +1,4 @@
+class ViewerSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes 
+end
---
+++ watchlist-backend/app/serializers/watchlist_serializer.rb
@@ -0,0 +1,4 @@
+class WatchlistSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes 
+end
